caba0d38b0342914d0605753c8ee123a8bab28fe
tech3br <dfsdireito95@gmail.com>
2025-07-18 02:57:56 -0300

    feat: adiciona listener para eventos de pedidos

---
+++ src/main/java/tech/tech3br/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,23 @@
+package tech.tech3br.orderms.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import tech.tech3br.orderms.listener.dto.OrderCreatedEvent;
+import static tech.tech3br.orderms.listener.dto.OrderCreatedEvent.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    // Este método será chamado quando uma mensagem for recebida na fila de pedidos criados.
+    // A anotação @Component indica que esta classe é um componente Spring, permitindo que o
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+        logger.info("Message consumed: {}", message);
+    }
+}
