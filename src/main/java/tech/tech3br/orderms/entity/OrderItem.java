a689a8da95381404bc674231715dcc3f674faf79
tech3br <dfsdireito95@gmail.com>
2025-07-19 01:52:26 -0300

    feat: adiciona entidade OrderItem com suporte a BigDecimal

--- src/main/java/tech/tech3br/orderms/entity/OrderItem.java
+++ src/main/java/tech/tech3br/orderms/entity/OrderItem.java
@@ -14,7 +14,10 @@ public class OrderItem {
     @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal price;
 
-    public OrderItem() {
+    public OrderItem(String product, Integer quantity, BigDecimal price) {
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
     }
 
     public String getProduct() {
